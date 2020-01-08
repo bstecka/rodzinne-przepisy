d2754e9f107ab0328549da0f2aadf547bbc0f5da
Barbara <bstecka@gmail.com>
2020-01-08 19:47:29 +0100

    Sort sale store list by price

--- src/SaleList.js
+++ src/SaleList.js
@@ -14,7 +14,7 @@ export default function SaleList({handleSalesClick, sales}) {
     renderItem={item => 
         <List.Item onClick={() => onClick(item.id)}>
             <Card hoverable cover={<img alt="sales_example" src={item.product_url} />}>{item.product_name}
-                <List itemLayout="horizontal" dataSource={item.stores.sort((a, b) => a.price - b.price)} size="large" renderItem={item => <List.Item>
+                <List itemLayout="horizontal" dataSource={this.state.sale.stores && this.state.sale.stores.sort((a, b) => a.price - b.price)} size="large" renderItem={item => <List.Item>
                       <List.Item.Meta width={150} avatar={<Avatar src={item.logo_url} shape="square" />} title={item.name} />
                       <div>{item.price} zł</div>
                     </List.Item>} />
