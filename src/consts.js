3b837a88b5d6902df4c563f0428d60d91635c723
Barbara <bstecka@gmail.com>
2020-01-16 13:09:25 +0100

    Change def url

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,3 @@
 export const apiURL = 'http://localhost:3000';
 
-export const defaultImageURL = 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/tort-bezowo-ajerkoniakowy-szk222523.jpg';
+export const defaultImageURL = 'https://cdn.galleries.smcloud.net/t/galleries/gf-BeZ9-WAY4-D8dY_spaghetti-bolognese-664x442-nocrop.jpg';
